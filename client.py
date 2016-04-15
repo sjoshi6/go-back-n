8ffe8dc2f2e3c51f8965082be9a5a0a6e60279b2
sjoshi6 <sjoshi6@ncsu.edu>
2016-04-15 19:22:44 -0400

    Client window and server acks working

--- client.py
+++ client.py
@@ -1,13 +1,156 @@
-
 import socket
 import sys
+import pickle
+from threading import Thread
+from settings import *
 
-SERVER_IP = sys.argv[1]
-SERVER_PORT = int(sys.argv[2])
-MESSAGE = "Amey Atul Patwardhan!"
-FTP_FILE_NAME = sys.argv[3]
-WINDOW_SIZE = int(sys.argv[4])
-MSS = sys.argv[5]
+SERVER_IP = ""
+SERVER_PORT = ""
+FTP_FILE_NAME = ""
+WINDOW_SIZE = ""
+MSS = ""
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(MESSAGE.encode('utf-8'), (SERVER_IP, SERVER_PORT))
+
+def read_file(file_name):
+    file_byte_arr = []
+    with open(file_name, 'rb') as data_file:
+        while True:
+
+            one_byte = data_file.read(1)
+            if not one_byte:
+                break
+
+            file_byte_arr.append(one_byte)
+
+    return file_byte_arr
+
+
+def rdt_send(bytes_arr, mss):
+    for i in range(0, len(bytes_arr), mss):
+        yield bytes_arr[i:i + mss]
+
+
+def carry_around_add(a, b):
+    c = a + b
+    return (c & 0xffff) + (c >> 16)
+
+
+def generate_checksum(msg):
+    # Assume the msg block is even
+    odd = False
+    if 1 == len(msg) % 2:
+        odd = True
+
+    sumation = 0
+
+    if odd:
+        for i in range(0, len(msg), 1):
+            w = ord('0') + (ord(msg[i]) << 8)
+            sumation = carry_around_add(sumation, w)
+
+    elif not odd:
+        for i in range(0, len(msg), 2):
+            w = ord(msg[i]) + (ord(msg[i + 1]) << 8)
+            sumation = carry_around_add(sumation, w)
+
+    return ~sumation & 0xffff
+
+
+def create_packets(bytes_arr, mss):
+    all_packets = []
+    sequence_no = 0
+
+    for segment in rdt_send(bytes_arr, mss):
+
+        packet = {"header": {}}
+        packet["header"]["sequence_number"] = sequence_no
+        packet["header"]["checksum"] = generate_checksum(segment)
+        packet["header"]["data_type"] = int('0101010101010101', 2)
+        packet["data"] = segment
+        all_packets.append(packet)
+        sequence_no += 1
+
+    return all_packets
+
+
+def ack_handler(ack_recv_sock):
+
+    global last_received_ack, finished
+    finished = False
+
+    while not finished:
+        data, addr = ack_recv_sock.recvfrom(1024)
+
+        ack_data = pickle.loads(data)
+        print(ack_data["header"]["ack_number"])
+
+        last_received_ack = ack_data["header"]["ack_number"]
+
+    ack_recv_sock.close()
+
+
+def main(ack_recv_sock):
+
+    try:
+        global final_sequence_no, last_sent_sequence_number, next_sequence_number, last_ack_number, current_window_size
+        global last_received_ack, finished
+
+        finished = False
+        last_sent_sequence_number = -1
+        next_sequence_number = 0
+        last_ack_number = -1
+        last_received_ack = -1
+
+        current_window_size = WINDOW_SIZE
+
+        # Create a socket to connect to server
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(('', CLIENT_SEND_PORT))
+
+        # Create a byte array of data to be sent
+        file_byte_arr = read_file(FTP_FILE_NAME)
+        packets = create_packets(file_byte_arr, MSS)
+
+        # last sequence number to expect
+        final_sequence_no = len(packets) - 1
+
+        # Unless we have reached the last packet
+        while int(last_received_ack) < int(final_sequence_no):
+
+            while (int(last_sent_sequence_number) - int(last_received_ack)) < int(WINDOW_SIZE):
+                packet = packets[last_sent_sequence_number+1]
+                last_sent_sequence_number += 1
+
+                data = pickle.dumps(packet)
+
+                sock.sendto(data, (SERVER_IP, SERVER_PORT))
+
+    finally:
+        # Once all acks are received close all
+        finished = True
+        sock.close()
+        sys.exit(1)
+
+
+def launcher():
+
+    ack_recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ack_recv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    ack_recv_sock.bind(('', CLIENT_ACK_PORT))
+
+    # Launch the ack handler in a separate thread
+    t = Thread(target=ack_handler, args=(ack_recv_sock,))
+    t.start()
+
+    # Then start the main function to send file from the current thread
+    main(ack_recv_sock)
+
+
+if __name__ == "__main__":
+    SERVER_IP = sys.argv[1]
+    SERVER_PORT = int(sys.argv[2])
+    FTP_FILE_NAME = sys.argv[3]
+    WINDOW_SIZE = int(sys.argv[4])
+    MSS = int(sys.argv[5])
+    launcher()
--- server.py
+++ server.py
@@ -1,33 +1,68 @@
 import socket
 import sys
+import pickle
 from random import uniform
+from settings import *
 
 SERVER_IP = "localhost"
-SERVER_PORT = int(sys.argv[1])
-FILE_NAME = sys.argv[2]
-PL_PROB = sys.argv[3]
+SERVER_PORT = ""
+FILE_NAME = ""
+PL_PROB = ""
 
 
 def prob_random_generator():
-	result=uniform(0,1)
-	print (result)
+
+    result = uniform(0, 1)
+    print(result)
+
+    return result
 
 
+def main():
 
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-server_socket= socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    try:
+        server_socket.bind((SERVER_IP, SERVER_PORT))
+        print("SERVER LISTENING ON PORT "+str(SERVER_PORT))
 
-try:
-	server_socket.bind((SERVER_IP,SERVER_PORT))
-	print ("SERVER LISTENING ON PORT 7735")
-except socket.error as msg:
-	print (msg)
-	sys.exit()
+        while True:
 
-while 1:
-	info,addr = server_socket.recvfrom(1024)
-	print ('Peer connected  ' + addr[0] + ':' + str(addr[1]))
-	print (info)
-	prob_random_generator()
-s.close()
+            data, addr = server_socket.recvfrom(1024)
+
+            # Extract client port details
+            client_ip = addr[0]
+            client_port = addr[1]
+            client_ack_port = int(client_port) + 1
+
+            packet = pickle.loads(data)
+            print(packet["data"])
+
+            ack = {"header": {}}
+            ack["header"]["data_type"] = int('0101010101010101', 2)
+            ack["header"]["bit_field"] = int('0000000000000000', 2)
+            ack["header"]["ack_number"] = packet["header"]["sequence_number"]
+
+            randm = prob_random_generator()
+
+            if randm > PL_PROB:
+                print("Discarded this packet")
+            else:
+                # Send the data and close this connection
+                server_socket.sendto(pickle.dumps(ack), (client_ip, client_ack_port))
+
+    except socket.error as msg:
+        print(msg)
+        sys.exit(1)
+
+    finally:
+        server_socket.close()
+        sys.exit(1)
+
+if __name__ == "__main__":
+
+    SERVER_PORT = int(sys.argv[1])
+    FILE_NAME = sys.argv[2]
+    PL_PROB = float(sys.argv[3])
+    main()
